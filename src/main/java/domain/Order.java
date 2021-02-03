d98928bd30250436a84b06f408cf6198ca2adabf
Dylan Mulder <45685819+pielemuis@users.noreply.github.com>
2021-02-03 17:55:16 +0100

    Finished up calculatePrice

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -3,12 +3,13 @@ package domain;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Order
 {
     private int orderNr;
     private boolean isStudentOrder;
-
+    private boolean isWeekend;
     private ArrayList<MovieTicket> tickets;
 
     public Order(int orderNr, boolean isStudentOrder)
@@ -31,20 +32,13 @@ public class Order
 
     public double calculatePrice()
     {
-        double totalPrice = 0;
+        //Arraylist for storing ticket prices
+        ArrayList<Double> ticketPrices = new ArrayList<Double>();
 
-        //TODO 1: Add functionality for 2nd ticket free
-        //In de winkel is bij 2e gratis het goedkoopste product gratis
-        //Dus ik gok dat wanneer je 3 normale en 3 premium tickets hebt de normale tickets gratis zijn
-        //Als dit het geval is moeten we misschien een aparte Arraylist met ticketprijzen maken,
-        // en hier de goedkoopste tickets uit halen wanneer de korting van toepassing is
-
-        //Functionality for getting day of the week for the screening.
-        DayOfWeek screeningDay = DayOfWeek.from(tickets.get(0).getScreeningDate());
-
-        //FUNCTIONALITY FOR GETTING BASE TICKET PRICE
-        //& ADDING THE APPROPRIATE PREMIUM PRICE TO THAT BASE WHERE APPLICABLE
+        //Get base ticket price
         double ticketBasePrice= tickets.get(0).getPrice();
+
+        //Add appropriate premium, add each ticket price to prices list
         double ticketPrice;
         for (MovieTicket movieTicket: tickets) {
             ticketPrice=ticketBasePrice;
@@ -55,12 +49,41 @@ public class Order
                 }
                 else ticketPrice += 3;
             }
-        //TODO 2: process this price, bearing in mind 2nd ticket free
-
+            ticketPrices.add(ticketPrice);
         }
 
+        //Functionality for getting day of the week for the screening.
+        DayOfWeek screeningDay = tickets.get(0).getScreeningDate().getDayOfWeek();
+        //TODO: Find a better alternative to this if statement
+        if (screeningDay==DayOfWeek.FRIDAY ||
+            screeningDay==DayOfWeek.SATURDAY ||
+            screeningDay==DayOfWeek.SUNDAY )
+        {
+            isWeekend=true;
+        }
 
-        return totalPrice;
+        if(isStudentOrder || !isWeekend) {
+            //Sort prices list by price(low to high) & remove cheapest 2nd free tickets
+            Collections.sort(ticketPrices);
+            int secondFree = ticketPrices.size() / 2;
+            ticketPrices.subList(0, secondFree).clear();
+        }
+
+        //Calculate total price
+        double total=0;
+        for (Double d : ticketPrices) {
+            total+=d;
+        }
+
+        //10% group discount for non-students during the weekends
+        //IMPORTANT: Ik ga er van uit dat de groepskorting niet samengaat met het 2e kaartje gratis.
+        //Hierom worden studenten niet meegenomen in de korting, en niet-studenten alleen in het weekend.
+        //Dit is hoe ik de opdracht heb begrepen. Verander de onderstaande functie als dit niet het geval is.
+        if (!isStudentOrder && isWeekend && tickets.size()>5){
+            total*=0.9;
+        }
+
+        return total;
     }
 
     public void export(TicketExportFormat exportFormat)
